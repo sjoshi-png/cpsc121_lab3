5cb0eb40a30c972b60a4112104922c1915404013
Saloni Joshi <sjoshi270@csu.fullerton.edu>
2023-09-13 13:48:47 -0700

    yay

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,13 +1,10 @@
-// TODO: please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Saloni Joshi
+// CPSC 121L-06
+// 09-06-2023
+// sjoshi270@csu.fullerton.edu
+// sjoshi-png
 //
 // Lab 3-1
-// If it is a pair programming lab please specify partner below.
-// Partner: @peteranteater
 
 #include <iostream>
 #include <string>
@@ -19,9 +16,7 @@ int main() {
   std::cout << "Please enter the time in military time: ";
   std::cin >> military_time;
 
-  // TODO: Call your function to convert from military time to regular time
-  // and assign its result to regular_time.
-  std::string regular_time;
+  std::string regular_time = MilitaryToRegularTime(military_time);
 
   std::cout << "The equivalent regular time is: " << regular_time << "\n";
   return 0;
--- prob01/time_converter.cc
+++ prob01/time_converter.cc
@@ -1,19 +1,42 @@
-// TODO: please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Saloni Joshi
+// CPSC 121L-06
+// 09-06-2023
+// sjoshi270@csu.fullerton.edu
+// sjoshi-png
 //
 // Lab 3-1
-// If it is a pair programming lab please specify partner below.
-// Partner: @peteranteater
 
 #include <iostream>
 #include <string>
 
 std::string MilitaryToRegularTime(int military_time) {
-  // TODO: convert military_time to regular time in string format.
-  // Hint: std::to_string() converts a given integer to a string.
-  return "";
+  int minutes = military_time % 100;
+  int military_hours = military_time / 100;
+
+  int regular_hours{2};
+  if (military_hours > 12) {
+    regular_hours = military_hours - 12;
+  } else if (military_hours == 0) {
+    regular_hours = 12;
+  } else {
+    regular_hours = military_hours;
+  }
+
+  std::string am_pm;
+  if (military_hours >= 12) {
+    am_pm = "pm";
+  } else {
+    am_pm = "am";
+  }
+
+  std::string regular_time;
+  if (minutes < 10) {
+    regular_time = std::to_string(regular_hours) + ":0" +
+                   std::to_string(minutes) + " " + am_pm;
+  } else {
+    regular_time = std::to_string(regular_hours) + ":" +
+                   std::to_string(minutes) + " " + am_pm;
+  }
+
+  return regular_time;
 }
